379a2e108a4e08ccca54b1252124d02806faa577
elul asfaha <scorpiondota22@gmail.com>
2023-08-17 14:19:09 +0300

    added error handler contact controller and error constants

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express")
-const dotenv = require("dotenv").config()
 const app = express()
+const dotenv = require("dotenv").config()
 const port = process.env.PORT || 5000
+const errorhandler = require("./middleware/errorHandler")
+app.use(express.json())
 app.use("/api/contacts", require("./routes/contactRoutes"))
+app.use(errorhandler)
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
 })
