2480f936b17448803e827918a3c53d188e32f311
elul asfaha <scorpiondota22@gmail.com>
2023-08-20 23:53:28 +0300

    added update and delete functionality

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -22,7 +22,7 @@ const createContact = asyncHandler(async (req, res) => {
         phoneNumber
     })
 
-    res.status(201).json(contact)
+    res.status(200).json(contact)
 })
 //@desc Get A contact
 //@route GET /api/contacts/:id
@@ -33,19 +33,37 @@ const getContact = asyncHandler(async (req, res) => {
         res.status(404)
         throw new Error("Contact not found")
     }
-    res.status(202).json(contact)
+    res.status(200).json(contact)
 })
 //@desc update a contact
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(202).json({ message: `Update contact for ${req.params.id}` })
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    const updateContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true
+        }
+    )
+    res.status(200).json(updateContact)
 })
 //@desc delete contact
 //@route delete /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-    res.status(202).json({ message: `Delete contact for ${req.params.id}` })
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    await Contact.remove()
+    res.status(200).json(contact)
 })
 module.exports = {
     getContacts,
