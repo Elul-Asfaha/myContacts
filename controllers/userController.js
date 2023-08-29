39547cbb8c417fcb426a7dfffe92ad25e2bf3d01
elul asfaha <scorpiondota22@gmail.com>
2023-08-29 15:26:01 +0300

    added a login function for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const User = require("../models/userModel")
 //@desc Register a user
 //@route Post /api/users/register
@@ -15,17 +17,48 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("User already registered")
     }
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await User.create({
+        username, email, password: hashedPassword
+    })
+    if (user) {
+        res.status(201).json({
+            _id: user.id, email: user.email
+        })
+    }
+    else {
+        res.status(400)
+        throw new Error("User data is not valid")
+    }
     res.json({
         message: "Register the user"
     })
 })
-//@desc Register a user
+//@desc login a user
 //@route Post /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({
-        message: "Login the user"
-    })
+    const { email, password } = req.body
+    if (!email || !password) {
+        res.status(400)
+        throw new Error("All fields are mandatory!")
+    }
+    const user = await User.findOne({ email })
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
+            }
+        }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1m" })
+        res.status(200).json({ accessToken })
+    } else {
+        res.status(401)
+    }
+    res.json({ message: "login user" })
 })
 //@desc Register a user
 //@route Post /api/users/current
